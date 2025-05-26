9b66df90982ca219af08e21b283243b41bdd83d6
pop-ecx <oliver.mulwa@protonmail.com>
2025-05-26 16:14:04 +0300

    feat: Invoke config functions in pickers to dynamically fetch pipelines and targets

--- lua/sigma_picker/picker.lua
+++ lua/sigma_picker/picker.lua
@@ -11,8 +11,37 @@ local M = {}
 M.sigma_picker = function(opts)
     opts = opts or {}
 
+    if not config.user_config or not config.user_config.sigma_cli_check then
+        vim.notify("sigma_cli_check function not found in sigma_picker.config.user_config", vim.log.levels.ERROR)
+        return
+    end
+
+    if not config.user_config.sigma_cli_check() then
+        return
+    end
+
+    local targets = config.user_config.get_sigma_targets()
+    if targets.error then
+        vim.notify(targets.error, vim.log.levels.ERROR)
+        return
+    end
+    if not next(targets) then
+        vim.notify("No Sigma backends available", vim.log.levels.WARN)
+        return
+    end
+
+    local sigma_rules = config.user_config.sigma_rules()
+    if not next(sigma_rules) then
+        vim.notify("No Sigma pipelines available", vim.log.levels.WARN)
+        return
+    end
+
     local function pick_config(selected_backend)
-        local configs = config.user_config.sigma_rules[selected_backend]
+        local configs = sigma_rules[selected_backend] or {}
+        if not next(configs) then
+            vim.notify("No pipelines available for " .. selected_backend, vim.log.levels.WARN)
+            return
+        end
 
         pickers.new(opts, {
             prompt_title = "Choose Configuration for " .. selected_backend,
